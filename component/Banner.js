ec9e43a2936023e49c9d9390bc57b96eaad592b8
thomas.florion <thomas.florion@epita.fr>
2024-05-08 17:50:42 +0200

    feat: updated tailwind config

--- component/Banner.js
+++ component/Banner.js
@@ -4,7 +4,7 @@ function Banner() {
   return (
     <div className="bg-gray-800 text-white text-center p-8 lg:p-8 bg-cover bg-banner-pattern">
       <h1 className="text-4xl lg:text-6xl font-bold mb-4">
-        Schmietta &apos; s Portfolio
+        Schmietta&apos;s Portfolio
       </h1>
       <p className="text-xl lg:text-2xl">A showcase of my work and skills</p>
     </div>
